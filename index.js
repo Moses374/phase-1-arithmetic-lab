05e60e87bb6927b277562d38434d42e7a725533c
Moses <emperorskid@gmail.com>
2024-03-23 15:38:52 +0300

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1=31
+const num2=2
+const muiltiply=(num1*num2);
+console.log(muiltiply)
+
+const random=Math.round(Math.random()*10);
+console.log(random)
+
+const num3=9
+const num4=5
+let mod=(num3%num4);
+console.log(mod)
+
+const max=Math.max(4,5,12,19,20);
+console.log(max)
